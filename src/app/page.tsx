131e2f03c7cc7c5fc4c14c0d6410ae52d6a268a6
fullMoonShade <flyinghuming.6000@gmail.com>
2024-12-30 19:41:40 +0600

    Added home.tsx and fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Navbar } from "@/components/navbar"
+import Home from "@/pages/home"
 
-export default function Home() {
+export default function Page() {
     return (
         <html lang="en">
           <body>
             <Navbar />
+            <Home />
           </body>
         </html>
       )
