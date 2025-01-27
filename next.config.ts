01f8705ef7a03e331822653d7002ef3e65e538a1
Shei <72736290+fullMoonShade@users.noreply.github.com>
2025-01-27 14:53:07 +0600

    hydration-overlay setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const {
+  withHydrationOverlay,
+} = require("@builder.io/react-hydration-overlay/next");
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = withHydrationOverlay({
+  /**
+   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+   */
+  appRootSelector: "main",
+})
 
-export default nextConfig;
